5dfae8770682895a8e4ffb950d514eab7dec7263
Tokana <opata123456@gmail.com>
2021-05-11 17:41:16 +0300

    Initial commit

---
+++ src/main/java/com/fire_app/fire_app/FireAppApplication.java
@@ -0,0 +1,13 @@
+package com.fire_app.fire_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fire_app/fire_app/FireAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fire_app.fire_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
